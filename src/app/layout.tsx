a32226c624255d322380f7c259dc683f14d8fb54
Fire Base <med.itrack199@gmail.com>
2025-08-31 08:56:17 +0000

    estas son las reglas que tiene, no se si aecte ena lgo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/med-itrack-nduik.appspot.com/o/app-logo.png?alt=media" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
